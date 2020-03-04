8f6bd7cb4f6a93adb129296ec32862e03e0c0715
Matthias Plancke <matthiasplancke@MacBook-Air-van-Matthias.local>
2020-03-04 20:17:35 +0000

    Fourth Commit

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Matthias Plancke on 01/03/2020.
+//  Copyright © 2020 Matthias Plancke. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
